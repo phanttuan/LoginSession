1ddbd2143c05dc13c080765a5691034b853b1a68
phanttuan <tuanphan19405@gmail.com>
2025-08-22 21:53:49 +0700

    Sửa lỗi login đúng không điều hướng

--- LoginSession/src/main/java/loginsession/LoginServlet.java
+++ LoginSession/src/main/java/loginsession/LoginServlet.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
